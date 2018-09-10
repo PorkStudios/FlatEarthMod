c90cb4a949917b81d7a91c042979c6bdca0419f7
DaMatrix <joey.rabil@gmail.com>
2018-09-10 20:42:22 +0200

    ultimate smoothing thing!

--- src/main/java/net/daporkchop/realmapcc/generator/HeightmapGenerator.java
+++ src/main/java/net/daporkchop/realmapcc/generator/HeightmapGenerator.java
@@ -1,8 +1,11 @@
 package net.daporkchop.realmapcc.generator;
 
+import com.mortennobel.imagescaling.AdvancedResizeOp;
 import com.mortennobel.imagescaling.ResampleFilter;
 import com.mortennobel.imagescaling.ResampleFilters;
 import com.mortennobel.imagescaling.ResampleOp;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 import net.daporkchop.lib.binary.NBitArray;
 import net.daporkchop.lib.db.DBBuilder;
 import net.daporkchop.lib.db.DatabaseFormat;
@@ -16,12 +19,15 @@ import net.daporkchop.realmapcc.generator.dataset.surface.cover.GlobCover;
 import net.daporkchop.realmapcc.util.ImageUtil;
 import net.daporkchop.realmapcc.util.KeyHasherChunkPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
 import smile.interpolation.BilinearInterpolation;
 import smile.interpolation.Interpolation2D;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -251,13 +257,17 @@ public class HeightmapGenerator implements Constants {
             GlobCover globCover = GlobCover.INSTANCE;
             NBitArray biomes = globCover.getDataAtDegree(-90, 50);
             BufferedImage reference = new BufferedImage(origSize, origSize, BufferedImage.TYPE_INT_RGB);
+            IntList occuringIDs = new IntArrayList();
             for (int x = origSize - 1; x >= 0; x--) {
                 for (int y = origSize - 1; y >= 0; y--) {
                     if (false) {
                         reference.setRGB(x, y, BiomeColor.values()[biomes.get(x * GLOBCOVER_valuesPerDegree + y)].color);
                     } else {
-                        if (false) {
+                        if (true) {
                             int col = biomes.get(x * GLOBCOVER_valuesPerDegree + y);
+                            if (!occuringIDs.contains(col)) {
+                                occuringIDs.add(col);
+                            }
                             reference.setRGB(x, y, (col << 16) | (col << 8) | col);
                         } else {
                             int col = biomes.get(x * GLOBCOVER_valuesPerDegree + y) * 10;
@@ -267,161 +277,78 @@ public class HeightmapGenerator implements Constants {
                 }
             }
 
-            /*ResampleOp resampler = new ResampleOp(origSize, origSize * sizeMult);
-            resampler.setFilter(ResampleFilters.getBSplineFilter());
-            BufferedImage target = resampler.filter(reference, null);
-            for (int x = origSize - 1; x >= 0; x--) {
-                for (int y = origSize - 2; y >= 0; y--) {
-                    int colA = reference.getRGB(x, y);
-                    int colB = reference.getRGB(x, y + 1);
-                    double diff = colA - colB;
-                    for (int yy = sizeMult - 1; yy >= 0; yy--)   {
-                        int col = target.getRGB(x, y * sizeMult + yy);
-                        double factor = Math.round((double) (col - colB) / diff);
-                        if (factor == 0.0d) {
-                            target.setRGB(x, y * sizeMult + yy, colB);
-                        } else if (factor == 1.0d)  {
-                            target.setRGB(x, y * sizeMult + yy, colA);
-                        } else {
-                            throw new IllegalStateException(String.valueOf(factor));
-                        }
-                    }
-                }
-            }*/
-
-            /*ResampleOp resampler = new ResampleOp(origSize, origSize * sizeMult);
-            resampler.setFilter(ResampleFilters.getBSplineFilter());
-            BufferedImage target = resampler.filter(reference, null);
+            BufferedImage target;
 
             if (true) {
-                for (int x = origSize - 1; x >= 0; x--) {
-                    for (int y = origSize - 2; y >= 0; y--) {
-                        int colA = reference.getRGB(x, y);
-                        int colB = reference.getRGB(x, y + 1);
-                        int rA = (colA >> 16) & 0xFF;
-                        int gA = (colA >> 8) & 0xFF;
-                        int bA = colA & 0xFF;
-                        int rB = (colB >> 16) & 0xFF;
-                        int gB = (colB >> 8) & 0xFF;
-                        int bB = colB & 0xFF;
-                        {
-                            rA = floorI(rA * 0.1d) * 10;
-                            gA = floorI(gA * 0.1d) * 10;
-                            bA = floorI(bA * 0.1d) * 10;
-                            rB = floorI(rB * 0.1d) * 10;
-                            gB = floorI(gB * 0.1d) * 10;
-                            bB = floorI(bB * 0.1d) * 10;
+                {
+                    ResampleOp resampler = new ResampleOp(origSize * sizeMult, origSize * sizeMult);
+                    resampler.setFilter(ResampleFilters.getBSplineFilter());
+                    resampler.setNumberOfThreads(Runtime.getRuntime().availableProcessors());
+                    BufferedImage bufferIn = new BufferedImage(origSize, origSize, BufferedImage.TYPE_INT_RGB);
+                    BufferedImage bufferOut = null;
+                    target = new BufferedImage(origSize * sizeMult, origSize * sizeMult, BufferedImage.TYPE_INT_RGB);
+                    for (int id : occuringIDs) {
+                        for (int x = origSize - 1; x >= 0; x--) {
+                            for (int y = origSize - 1; y >= 0; y--) {
+                                int col = 0;
+                                if ((reference.getRGB(x, y) & 0xFF) == id) {
+                                    col = 0xFF;
+                                }
+                                bufferIn.setRGB(x, y, col);
+                            }
                         }
-                        double diffR = rA - rB;
-                        double diffG = gA - gB;
-                        double diffB = bA - bB;
-                        for (int yy = sizeMult - 1; yy >= 0; yy--) {
-                            int col = target.getRGB(x, y * sizeMult + yy);
-                            int r = (col >> 16) & 0xFF;
-                            int g = (col >> 8) & 0xFF;
-                            int b = col & 0xFF;
-                            col = 0;
-                            if (diffR == 0) {
-                                col |= rA << 16;
-                            } else {
-                                double factorR = Math.abs((double) (r - rB) / diffR);
-                                if (factorR < 0.5d) {
-                                    col |= rB << 16;
-                                } else {
-                                    col |= rA << 16;
+
+                        bufferOut = resampler.filter(bufferIn, bufferOut);
+
+                        int col = BiomeColor.values()[id].color;
+                        for (int x = origSize * sizeMult - 1; x >= 0; x--) {
+                            for (int y = origSize * sizeMult - 1; y >= 0; y--) {
+                                if ((bufferOut.getRGB(x, y) & 0xFF) >= 128) {
+                                    target.setRGB(x, y, col);
                                 }
                             }
-                            if (diffG == 0) {
-                                col |= gA << 8;
-                            } else {
-                                double factorG = Math.abs((double) (g - gB) / diffG);
-                                if (factorG < 0.5d) {
-                                    col |= gB << 8;
-                                } else {
-                                    col |= gA << 8;
-                                }
-                            }
-                            if (diffB == 0) {
-                                col |= bA;
-                            } else {
-                                double factorB = Math.abs((double) (b - gB) / diffB);
-                                if (factorB < 0.5d) {
-                                    col |= bB;
-                                    //col = 0xFF5555;
-                                } else {
-                                    col |= bA;
-                                    //col = 0x55FF55;
-                                }
-                            }
-                            target.setRGB(x, y * sizeMult + yy, col);
                         }
                     }
                 }
-            }*/
+                System.gc();
+                {
+                    BufferedImage newTarget = deepCopy(target);
+                    for (int x = origSize * sizeMult - 1; x >= 0; x--) {
+                        for (int y = origSize * sizeMult - 1; y >= 0; y--) {
+                            int col = target.getRGB(x, y);
+                            if ((col & 0xFFFFFF) == 0) {
+                                int X = sizeMult << 1;
+                                int Y = sizeMult << 1;
+                                int xx = 0, yy = 0, dx = 0, dy = -1;
+                                int t = Math.max(X, Y);
+                                int maxI = t * t;
 
-            ResampleOp resampler = new ResampleOp(origSize * sizeMult, origSize * sizeMult);
-            resampler.setFilter(ResampleFilters.getBSplineFilter());
-            BufferedImage target = resampler.filter(reference, null);
-            for (int x = origSize - 2; x >= 0; x--) {
-                for (int y = origSize - 2; y >= 0; y--) {
-                    int colA = reference.getRGB(x, y) & 0xFF;
-                    int colB = reference.getRGB(x, y + 1) & 0xFF;
-                    int colC = reference.getRGB(x + 1, y) & 0xFF;
-                    int colD = reference.getRGB(x + 1, y + 1) & 0xFF;
-                    Interpolation2D interp = new BilinearInterpolation(
-                            new double[]{0.0d, 1.0d},
-                            new double[]{0.0d, 1.0d},
-                            new double[][]{
-                                    new double[]{colA, colB},
-                                    new double[]{colC, colD}
-                            }
-                    );
-                    double diffR = rA - rB;
-                    double diffG = gA - gB;
-                    double diffB = bA - bB;
-                    for (int xx = sizeMult - 1; xx >= 0; xx--) {
-                        for (int yy = sizeMult - 1; yy >= 0; yy--) {
-                            double variationFromA = interp.interpolate(xx / 10.0d, yy / 10.0d);
-                            int col = target.getRGB(x, y * sizeMult + yy);
-                            int r = (col >> 16) & 0xFF;
-                            int g = (col >> 8) & 0xFF;
-                            int b = col & 0xFF;
-                            col = 0;
-                            if (diffR == 0) {
-                                col |= rA << 16;
-                            } else {
-                                double factorR = (double) (r - rB) / diffR;
-                                if (factorR < 0.0d) {
-                                    col |= rB << 16;
-                                } else {
-                                    col |= rA << 16;
+                                A:
+                                for (int i = 0; i < maxI; i++) {
+                                    if ((-X / 2 <= xx) && (xx <= X / 2) && (-Y / 2 <= yy) && (yy <= Y / 2)) {
+                                        col = target.getRGB(
+                                                MathHelper.clamp(x + xx, 0, origSize * sizeMult - 1),
+                                                MathHelper.clamp(y + yy, 0, origSize * sizeMult - 1)
+                                        );
+                                        if ((col & 0xFFFFFF) != 0) {
+                                            newTarget.setRGB(x, y, col);
+                                            break A;
+                                        }
+                                    }
+
+                                    if ((xx == yy) || ((xx < 0) && (xx == -yy)) || ((xx > 0) && (xx == 1 - yy))) {
+                                        t = dx;
+                                        dx = -dy;
+                                        dy = t;
+                                    }
+                                    xx += dx;
+                                    yy += dy;
                                 }
                             }
-                            if (diffG == 0) {
-                                col |= gA << 8;
-                            } else {
-                                double factorG = (double) (g - gB) / diffG;
-                                if (factorG < 0.0d) {
-                                    col |= gB << 8;
-                                } else {
-                                    col |= gA << 8;
-                                }
-                            }
-                            if (diffB == 0) {
-                                col |= bA;
-                            } else {
-                                double factorB = (double) (b - gB) / diffB;
-                                if (factorB < 0.0d) {
-                                    col |= bB;
-                                    //col = 0xFF5555;
-                                } else {
-                                    col |= bA;
-                                    //col = 0x55FF55;
-                                }
-                            }
-                            target.setRGB(x, y * sizeMult + yy, col);
                         }
                     }
+                    target = newTarget;
+                    System.gc();
                 }
             }
 
@@ -446,6 +373,13 @@ public class HeightmapGenerator implements Constants {
         }
 
         System.exit(0);
+    }
+
+    static BufferedImage deepCopy(BufferedImage bi) {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
     }
 
     public static void tryConvert(ChunkPos pos, PorkDB<ChunkPos, CompactedHeightData> db) {
